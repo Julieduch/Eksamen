dc6b0f1a5b5e3e90d140e6f6974c9d485fe1474c
Julieduch <59617014+Julieduch@users.noreply.github.com>
2020-01-07 17:44:23 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Julieduch</title>
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="Style/Styles.css">
+</head>
+<body>
+    <main class="container col-lg-12">
+        <div class="row">
+                    <div id="sidebar" class="position-fixed col-2">
+                        <h2 class="text-right pr-5 pt-3">Julie<br>Duch<br>Lauridsen</h2>
+                        <nav class="navbar navbar-dark text-right pr-5">
+                            <ul class="navbar-nav col-lg-12 p-0">
+                                <li class="navbar-item">
+
+                                    <a class="nav-link" href="index.php">Forside 01</a>
+                                </li>
+
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="02webudvikling.html">Webudvikling 02</a>
+                                </li>
+
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="03interface.html">Interfacedesign 03</a>
+                                </li>
+
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="04kommunikation.html">Kommunikation 04</a>
+                                </li>
+                                
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="05virksomhed.html">Business model 05</a>
+                                </li>
+                                
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="06design.html">Design 06</a>
+                                </li>
+                                
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="07projekter.html">Projekter 07</a>
+                                </li>
+                                
+                                <li class="navbar-item">
+                                    <a class="nav-link" href="08omsiden.html">Om siden 08</a>
+                                </li>
+                                
+                            
+
+                            </ul>
+
+                        </nav>
+
+                    </div>
+
+
+
+                    <div class="col-lg-10 ml-auto">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/400/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/400/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/400/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/400/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/400/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/200/300?grayscale">
+                        <img class="pr-2 pt-3" src="https://picsum.photos/800/300?grayscale">
+
+                    </div>
+
+
+
+
+
+
+
+
+            </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
